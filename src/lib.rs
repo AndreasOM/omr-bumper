c91c35d5ae1194985768de929f49456e895ee544
Andreas Neukoetter <andreas@omni-mad.com>
2021-12-02 19:04:26 +0100

    + Add (minimal) documentation.

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,10 @@
+//! omr-bumper is not intended to be a library, but it can be embedded into your code
+//!
+//! Run `omr-bumper --help` for more info
+//!
+//! *Note:* This is work-in-progress, and _works for us_. Use with care!
+//!
+//! Sensible pull-requests, and issues welcome at: (https://github.com/AndreasOM/omr-bumper)
 
 
 mod release;
